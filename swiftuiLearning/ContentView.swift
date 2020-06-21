91d4ca7fe15877b6c0be0a962aab81e069ead967
alex-babych <ababich.dev@gmail.com>
2020-06-21 21:56:44 +0300

    Initial Commit

---
+++ swiftuiLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftuiLearning
+//
+//  Created by Alex Babich on 21.06.2020.
+//  Copyright © 2020 Alex Babich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
