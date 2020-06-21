8c0fc52771ccbfe23a866f9cd4ac7e8b6a595aa9
alex-babych <ababich.dev@gmail.com>
2020-06-21 23:03:21 +0300

    added HorizontalList from random list of emoji

--- swiftuiLearning/ContentView.swift
+++ swiftuiLearning/ContentView.swift
@@ -9,8 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, World!")
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(0..<self.generateData().count, id: \.self) { index in
+                    VStack {
+                        Text("\(self.generateData()[index])")
+                            .font(.system(size: 60))
+                            .foregroundColor(.blue)
+                            .fontWeight(.bold)
+                    }
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(20)
+                }
+            }
+        }
+    }
+
+    private func generateData() -> [String] {
+        var data: [String] = []
+        for _ in 0..<100 {
+            data.append(randomEmoji())
+        }
+
+        return data
+    }
+    
+    private func randomEmoji() -> String {
+        let range = 0x1F300...0x1F3F0
+        let index = Int(arc4random_uniform(UInt32(range.count)))
+        let ord = range.lowerBound + index
+        guard let scalar = UnicodeScalar(ord) else { return "❓" }
+        return String(scalar)
     }
 }
 
