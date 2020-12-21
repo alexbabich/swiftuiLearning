e786605e6cc01194595dfede8a8f8981311f61d8
alex-babich <ababich.dev@gmail.com>
2020-12-21 22:21:13 +0200

    Initial Commit

---
+++ Shared/AnimatedRatingsViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedRatingsViewSwiftUIApp.swift
+//  Shared
+//
+//  Created by alex-babich on 21.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedRatingsViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
